fef33c5c5f62c4fa8562ed9521932249d0cadd45
Alexey Mulyukin <adiott52@gmail.com>
2024-04-06 14:13:24 +0300

    init

---
+++ client/src/main/java/ru/mulyukin/otus/march/chat/client/ClientApplycation.java
@@ -0,0 +1,39 @@
+package ru.mulyukin.otus.march.chat.client;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
+public class ClientApplycation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(in);
+        try (
+                Socket socket = new Socket("localhost", 8189);
+                DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+                DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream())
+        ) {
+            System.out.println("Подключились к серверу");
+            new Thread(() -> {
+                try {
+                    while (true) {
+                        String inMessage = inputStream.readUTF();
+                        System.out.println(inMessage);
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }).start();
+            while (true) {
+                String messege = scanner.nextLine();
+                outputStream.writeUTF(messege);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ server/src/main/java/ru/mulyukin/otus/march/chat/server/ClientHandler.java
@@ -0,0 +1,73 @@
+package ru.mulyukin.otus.march.chat.server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientHandler {
+    private Server server;
+    private Socket socket;
+    private DataInputStream inputStream;
+    private DataOutputStream outputStream;
+    private String userName;
+
+    private static int userCounter = 0;
+
+    private void generateUsername(){
+        userCounter ++;
+        this.userName = "user" + userCounter;
+    }
+
+    public ClientHandler(Server server, Socket socket) throws IOException {
+        this.server = server;
+        this.socket = socket;
+        this.inputStream = new DataInputStream(socket.getInputStream());
+        this.outputStream = new DataOutputStream(socket.getOutputStream());
+        this.generateUsername();
+        new Thread(() -> {
+            try {
+                System.out.println("Подключился новый клиент");
+                while (true) {
+                    String messege = inputStream.readUTF();
+                    server.broadcastMessage(userName + ": " + messege);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }finally {
+                disconect();
+            }
+        }).start();
+    }
+    public void sendMessage(String messege){
+        try {
+            outputStream.writeUTF(messege);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    public void disconect(){
+        server.unSubscribe(this);
+        try {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        try {
+            if (outputStream != null) {
+                outputStream.close();
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        try {
+            if (socket != null && socket.isClosed()) {
+                socket.close();
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
